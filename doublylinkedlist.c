2c2a3130ee1ba7527635754506972464b062c06b
Pranshu kumar <123887342+Pranshu936@users.noreply.github.com>
2024-05-22 21:27:15 +0530

    Add files via upload

---
+++ doublylinkedlist.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the node structure
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+// Function to traverse the list forward
+void traverseForward(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+// Function to traverse the list backward
+void traverseBackward(struct Node *node) {
+    while (node->next != NULL) {
+        node = node->next;
+    }
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->prev;
+    }
+    printf("\n");
+}
+
+void insertAtBeginning(struct Node **headRef, int newData) {
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = newData;
+    newNode->prev = NULL;
+    newNode->next = *headRef;
+    if (*headRef != NULL) {
+        (*headRef)->prev = newNode;
+    }
+    *headRef = newNode;
+}
+
+void insertAtend(struct Node **headRef, int newData) {
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = newData;
+    newNode->next = NULL;
+    struct Node *last = *headRef;
+    while (last->next != NULL)
+        last = last->next;
+    last->next = newNode;
+    newNode->prev = last;
+}
+
+void insertAfter(struct Node *prevNode, int newData) {
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = newData;
+    newNode->next = prevNode->next;
+    newNode->prev = prevNode;
+    if (prevNode->next != NULL) {
+        prevNode->next->prev = newNode;
+    }
+    prevNode->next = newNode;
+}
+
+void deleteAtfirst(struct Node **headRef){
+    struct Node *temp = *headRef;
+    *headRef=(*headRef)->next;
+    (*headRef)->prev=NULL;
+}
+
+void deleteAtend(struct Node **headRef){
+    struct Node *temp=*headRef;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->prev->next=NULL;
+}
+
+void deleteAfter(struct Node *prevNode){
+    struct Node *temp = prevNode->next;
+    prevNode->next = temp->next;
+    if (temp->next != NULL) {
+        temp->next->prev = prevNode;
+    }
+
+}
+
+int main() {
+    struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* second = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* third = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 1;
+    head->next = second;
+    head->prev = NULL;
+
+    second->data = 2;
+    second->next = third;
+    second->prev = head;
+
+    third->data = 3;
+    third->next = NULL;
+    third->prev = second;
+
+    insertAtBeginning(&head, 5);
+    insertAtend(&head, 4);
+    insertAfter(head, 6);
+    deleteAtfirst(&head);
+    deleteAtend(&head);
+    deleteAfter(second);
+
+    printf("Traverse forward:\n");
+    traverseForward(head);
+
+    printf("Traverse backward:\n");
+    traverseBackward(head);
+
+    return 0;
+}
