bc6cca635764e4c18b058cc20355b7992d731726
Pranshu kumar <123887342+Pranshu936@users.noreply.github.com>
2024-07-05 04:10:49 +0530

    Update stack_ll.c

--- stack_ll.c
+++ stack_ll.c
@@ -1,76 +1,69 @@
-#include<stdio.h>
-#include<stdlib.h>
- 
-struct Node{
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
     int data;
-    struct Node * next;
+    struct Node* next;
 };
- 
+
 struct Node* top = NULL;
- 
-void linkedListTraversal(struct Node *ptr)
-{
-    while (ptr != NULL)
-    {
+
+void linkedListTraversal(struct Node* ptr) {
+    while (ptr != NULL) {
         printf("Element: %d\n", ptr->data);
-        ptr = ptr->next; 
+        ptr = ptr->next;
     }
 }
- 
-int isEmpty(struct Node* top){
-    if (top==NULL){
-        return 1;
-    }
-    else{
-        return 0;
-    }
+
+int isEmpty(struct Node* top) {
+    return top == NULL;
 }
- 
-int isFull(struct Node* top){
-    struct Node* p = (struct Node*)malloc(sizeof(struct Node));
-    if(p==NULL){
-        return 1;
-    }
-    else{
-        return 0;
-    }
+
+struct Node* push(struct Node* top, int x) {
+    struct Node* n = (struct Node*)malloc(sizeof(struct Node));
+    n->data = x;
+    n->next = top;
+    top = n;
+    return top;
 }
- 
-struct Node* push(struct Node* top, int x){
-    if(isFull(top)){
-        printf("Stack Overflow\n");
-    }
-    else{
-        struct Node* n = (struct Node*) malloc(sizeof(struct Node));
-        n->data = x;
-        n->next = top;
-        top = n;
-        return top;
-    }
-}
- 
-int pop(struct Node* tp){
-    if(isEmpty(tp)){
+
+int pop(struct Node* tp) {
+    if (isEmpty(tp)) {
         printf("Stack Underflow\n");
-    }
-    else{
+        return -1; // Return an error value
+    } else {
         struct Node* n = tp;
-        top = (tp)->next;
+        top = tp->next;
         int x = n->data;
         free(n);
-        return x; 
+        return x;
     }
 }
- 
-int main(){
+
+int peek(struct Node* tp) {
+    if (isEmpty(tp)) {
+        printf("Stack is empty\n");
+        return -1; // Return an error value
+    } else {
+        return tp->data;
+    }
+}
+
+int main() {
     top = push(top, 78);
     top = push(top, 7);
     top = push(top, 8);
-    
-     linkedListTraversal(top);
- 
-    int element = pop(top); 
+
+    linkedListTraversal(top);
+
+    int element = pop(top);
     printf("Popped element is %d\n", element);
     linkedListTraversal(top);
+
+    int topElement = peek(top);
+    if (topElement != -1) {
+        printf("Top element is %d\n", topElement);
+    }
+
     return 0;
 }
